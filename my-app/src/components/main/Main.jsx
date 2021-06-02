105541f52ee836ba1e6aa4c89d68c52e80592607
User <2176589@mail.ru>
2021-06-01 23:09:03 -0700

    Add table

--- my-app/src/components/main/Main.jsx
+++ my-app/src/components/main/Main.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import "./Main.less"
+import "./Main.css"
 import axios from "axios";
 
 const Main = () => {
@@ -7,7 +7,7 @@ const Main = () => {
   async function getData() {
     const response = await axios.get(` https://www.nbrb.by/api/exrates/rates?periodicity=0`)
     return response.data
-      }
+  }
 
   const [data, setData] = useState([]);
 
@@ -19,10 +19,38 @@ const Main = () => {
     fetchData();
   }, []);
 
+  function formatDate() {
+    const nowDate = new Date();
+
+    let dd = nowDate.getDate();
+    dd < 10 && (dd = '0' + dd);
+
+    let mm = nowDate.getMonth() + 1;
+    mm < 10 && (mm = '0' + mm);
+
+    return dd + '.' + mm + '.' + nowDate.getFullYear();
+  }
+
 
   return (
-    <div>
-      {data.map(el=><div>{el["Cur_Name"] + ":" + el["Cur_OfficialRate"]}</div>)}
+    <div className="container">
+      <div>
+        Официальный курс белорусского рубля по отношению к иностранным валютам,
+        устанавливаемый Национальным банком Республики Беларусь ежедневно, на {formatDate()}
+      </div>
+      <table>
+        <tr className="table_header">
+          <th className="table-title">Наименование иностранной валюты</th>
+          <th className="table-title">Количество единиц иностранной валюты, буквенный код валюты</th>
+          <th className="table-title">Официальный курс</th>
+        </tr>
+        {data.map(el =>
+          <tr>
+            <td>{el["Cur_Name"]}</td>
+            <td>{el["Cur_Scale"] + " " + el["Cur_Abbreviation"]}</td>
+            <td>{el["Cur_OfficialRate"]}</td>
+          </tr>)}
+      </table>
     </div>
   );
 };
